16c4b11a606fa0e983698398d13ae3f2ebe6f976
kwon5700 <kwon777071@gmail.com>
2024-10-01 19:32:33 +0900

    태초 마을

--- Gomdoliro-front/src/main.jsx
+++ Gomdoliro-front/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Header from './components/Header.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Header />}/>
+        </Routes>
+    </BrowserRouter>
 )
