22ea1158e880f0504018f80946b3241463f8337c
kwon5700 <kwon777071@gmail.com>
2024-10-01 23:14:26 +0900

    Feat : Header 컴포넌트 개발

--- Gomdoliro-front/src/components/Header.jsx
+++ Gomdoliro-front/src/components/Header.jsx
@@ -4,7 +4,18 @@ import '../styles/header.css'
 const Header = () => {
     return (
         <header>
-            
+            <h2>BamGallery</h2>
+            <span className="search_box">
+                <label htmlFor="text"/>
+                <input type="text" placeholder="검색어를 입력하세요"/>
+                <img src="src/assets/search.png" alt="search"/>
+            </span>
+            <span className="user_list">
+                <h3>내 게시물</h3>
+                <img src="src/assets/user_line.png" className="user_line" alt="user_line"/>
+                <h3>Jin_venus08</h3>
+                <img src="src/assets/user_icon.png" className="user_ion" alt="user_icon"/>
+            </span>
         </header>
     )
 }
