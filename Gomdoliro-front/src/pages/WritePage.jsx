374e1c9f70dcfe48dabfccba3b88131e6f6f9c34
kwon5700 <kwon777071@gmail.com>
2024-10-03 02:18:13 +0900

    post api 연동 끝

--- Gomdoliro-front/src/pages/WritePage.jsx
+++ Gomdoliro-front/src/pages/WritePage.jsx
@@ -1,10 +1,25 @@
 import {React, useState} from 'react'
+import axios from 'axios'
 import Write from '../components/Write'
 import Write_underbar from '../components/Write_underbar'
 
 const WritePage = () => {
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
+
+    const post = async () => {
+        try {
+            const response = await axios.post('https://port-0-gomdoliro-board-back-m1qhzohka7273c65.sel4.cloudtype.app/board' , {
+                title,
+                content
+            })
+            console.log('request success')
+            setTitle('')
+            setContent('')
+        } catch (error) {
+            console.error('Error : ', error)
+        }
+    }
 
     const handleInputChange = (e) => {
         setTitle(e.target.value)
@@ -17,9 +32,7 @@ const WritePage = () => {
     const handleSubmit = () => {
         if(title.length === 0) alert("제목을 입력해주세요.")
         else if(content.length === 0) alert("내용을 입력해주세요.")
-        else {
-            
-        }
+        else post()
     }
 
     return (
