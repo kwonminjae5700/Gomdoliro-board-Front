d073229d94ede564c0624f0ed4c09756c09c899f
kwon5700 <kwon777071@gmail.com>
2024-10-03 21:41:15 +0900

    Chore : Modal button 컴포넌트 props 수정 -> 취소

--- Gomdoliro-front/src/components/Modal.jsx
+++ Gomdoliro-front/src/components/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({onClose, delIsFinish}) => {
             <div className="modal_box">
                 <h2>이 글을 삭제하시겠습니까?</h2>
                 <div>
-                    <Read_button color={"#999999"} text={"수정"} button_work={onClose}/>
+                    <Read_button color={"#999999"} text={"취소"} button_work={onClose}/>
                     <Read_button color={"#FF1F1F"} text={"삭제"} button_work={delIsFinish}/>
                 </div>
             </div>
