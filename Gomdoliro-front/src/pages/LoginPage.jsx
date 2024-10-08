875a4def4044e1e059380e07c49288433498fa12
kwon5700 <kwon777071@gmail.com>
2024-10-08 22:03:50 +0900

    Feat : 로그인 페이지 퍼블리싱

--- Gomdoliro-front/src/pages/LoginPage.jsx
+++ Gomdoliro-front/src/pages/LoginPage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {React, useState} from 'react'
 import '../styles/login.css'
 
 const LoginPage = () => {
+    const [userId, setUserId] = useState('')
+    const [userPW, setUserPW] = useState('')
 
+    let buttonStyle = {
+        backgroundColor : "#999999"
+    }
+
+    if(userId.length > 0 && userPW.length > 0) {
+        buttonStyle = {
+            backgroundColor : "#1F8BFF"
+        }
+    }
 
     return (
         <div className="loginBackground">
@@ -10,13 +21,28 @@ const LoginPage = () => {
                 <h1>Login</h1>
                 <div>
                     <div className="login-input">
-                        <input type="text" placeholder="아이디 입력"/>
+                        <input 
+                            type="text"    
+                            placeholder="이메일 입력"
+                            value={userId}
+                            onChange={(e) => {
+                                setUserId(e.target.value)
+                            }}
+                        />
                     </div>
                     <div className="login-input">
-                        <input type="text" placeholder="비밀번호 입력"/>
+                        <input 
+                            type="password" 
+                            placeholder="비밀번호 입력"
+                            value={userPW}
+                            onChange={(e) => {
+                                setUserPW(e.target.value)
+                            }}
+                        />
                     </div>
                 </div>
-                <button></button>
+                <button style={buttonStyle}>로그인</button>
+                <h3>회원가입하기</h3>
             </main>
         </div>
     )
