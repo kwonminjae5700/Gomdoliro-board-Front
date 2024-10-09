fc8cd24136b59861319d8ea9a8fbacefeda9c27a
kwon5700 <kwon777071@gmail.com>
2024-10-09 18:57:24 +0900

    Feat : 계정 생성 기능 개발 (닉네임 부분 개발 X)

--- Gomdoliro-front/src/pages/SignupPage.jsx
+++ Gomdoliro-front/src/pages/SignupPage.jsx
@@ -1,18 +1,41 @@
 import {React, useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 import '../styles/login.css'
 
 const SignupPage = () => {
-    const [userId, setUserId] = useState('')
-    const [userPW, setUserPW] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const nav = useNavigate()
 
+    const userPost = async () => {
+        try {
+            const response = await axios.post('https://port-0-gomdoliro-board-back-m1qhzohka7273c65.sel4.cloudtype.app/signup', {
+                email,
+                password
+            })
+            console.log('success')
+            setEmail('')
+            setPassword('')
+            nav("/")
+        } catch(error) {
+            console.error('Error', error)
+        }
+    }
+    
+    let canSignup = 0
     let buttonStyle = {
         backgroundColor : "#999999"
     }
 
-    if(userId.length > 0 && userPW.length > 0) {
+    if(email.length > 0 && password.length > 0) {
         buttonStyle = {
             backgroundColor : "#1F8BFF"
         }
+
+        canSignup = 1
+    } else {
+        canSignup = 0
     }
 
     return (
@@ -24,9 +47,9 @@ const SignupPage = () => {
                         <input 
                             type="text"    
                             placeholder="이메일 입력"
-                            value={userId}
+                            value={email}
                             onChange={(e) => {
-                                setUserId(e.target.value)
+                                setEmail(e.target.value)
                             }}
                         />
                     </div>
@@ -34,15 +57,21 @@ const SignupPage = () => {
                         <input 
                             type="password" 
                             placeholder="비밀번호 입력"
-                            value={userPW}
+                            value={password}
                             onChange={(e) => {
-                                setUserPW(e.target.value)
+                                setPassword(e.target.value)
                             }}
                         />
                     </div>
                 </div>
-                <button style={buttonStyle}>회원가입</button>
-                <h3>로그인하기</h3>
+                <button style={buttonStyle} onClick={() => {
+                    if(canSignup) {
+                        userPost()
+                    }
+                }}>회원가입</button>
+                <h3 onClick={() => {
+                    nav("/")
+                }}>로그인하기</h3>
             </main>
         </div>
     )
