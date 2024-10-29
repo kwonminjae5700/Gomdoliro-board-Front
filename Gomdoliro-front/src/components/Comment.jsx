93317ee2040d3838708b7b5c56791d8f9c23bc0c
kwon5700 <kwon777071@gmail.com>
2024-10-29 15:56:23 +0900

    Feat : 댓글 읽기 API 연동

--- Gomdoliro-front/src/components/Comment.jsx
+++ Gomdoliro-front/src/components/Comment.jsx
@@ -3,19 +3,19 @@ import '../styles/comment.css';
 import user_icon from '../assets/user_icon.png';
 import seroJum from '../assets/seroJum.png';
 
-const Comment = () => {
+const Comment = ({writer, content}) => {
     return (
         <div className="commentBox">
             <span className="commentRow">
                 <img src={user_icon} alt="icon" className="userIcon" />
                 <span className="commentSpan">
-                    <h3>Devener</h3>
+                    <h3>{writer}</h3>
                     <h4>2024.10.21</h4>
                 </span>
                 <img src={seroJum} alt="seroJum" className="seroJum" />
             </span>
             <div className="commentContent">
-                hello
+                {content}
             </div>
             <div className="addComment">
                 + 답글 달기
--- Gomdoliro-front/src/components/CommentBox.jsx
+++ Gomdoliro-front/src/components/CommentBox.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import axios from 'axios'
 import '../styles/commentBox.css'
+import Comment from '../components/Comment'
 
-const CommentBox = () => {
+const CommentBox = ({boardId}) => {
+    const server = import.meta.env.VITE_SERVER_ADDRESS
+    const [comments, setComments] = useState([])
     
+    useEffect(() => {
+        const getAll = async () => {
+            try {
+                const response = await axios.get(`${server}/board/${boardId}/comments/all`)
+                setComments(response.data)
+            } catch(error) {
+                console.error('Error : ', error);
+            }
+        }
+
+        getAll()
+    }, [])
+
+    console.log(comments)
+
     return (
         <div className="comment">
             <div className="comment_box">
                 <textarea placeholder="댓글 작성" />
             </div>
-            <button>작성하기</button>
+            <span className="buttonBox">
+                <button>취소</button>
+                <button>작성</button>
+            </span>
             <div className="userComment">
-                <h2>아직 댓글이 없습니다.</h2>
+                {comments.length === 0 && <h2>아직 댓글이 없습니다</h2>}
+                {comments.map((comment) => (
+                    <Comment writer={comment.commentWriter} content={comment.commentContent}/>
+                ))}
             </div>
         </div>
     )
