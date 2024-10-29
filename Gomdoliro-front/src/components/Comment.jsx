21c82ef706cd27f3a7b10b846569229f5d9c10b5
kwon5700 <kwon777071@gmail.com>
2024-10-29 13:19:29 +0900

    Feat : 댓글 컴포넌트 퍼블리싱

--- Gomdoliro-front/src/components/Comment.jsx
+++ Gomdoliro-front/src/components/Comment.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import '../styles/comment.css'
-import user_icon from '../assets/user_icon.png'
+import React from 'react';
+import '../styles/comment.css';
+import user_icon from '../assets/user_icon.png';
+import seroJum from '../assets/seroJum.png';
 
 const Comment = () => {
-    
-
     return (
-        <div>
-            <img src={user_icon} alt="icon"/>
-            
+        <div className="commentBox">
+            <span className="commentRow">
+                <img src={user_icon} alt="icon" className="userIcon" />
+                <span className="commentSpan">
+                    <h3>Devener</h3>
+                    <h4>2024.10.21</h4>
+                </span>
+                <img src={seroJum} alt="seroJum" className="seroJum" />
+            </span>
+            <div className="commentContent">
+                hello
+            </div>
+            <div className="addComment">
+                + 답글 달기
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Comment
+export default Comment;
