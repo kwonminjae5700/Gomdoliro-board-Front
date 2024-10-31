8c043e3b34ccfd0162400286d6dbcdc8ee8cf9da
kwon5700 <kwon777071@gmail.com>
2024-10-31 18:46:40 +0900

    Fix : 다시 롤백

--- Gomdoliro-front/src/components/Posts.jsx
+++ Gomdoliro-front/src/components/Posts.jsx
@@ -1,11 +1,12 @@
-    import {React, useEffect, useState} from 'react'
-    import Post from './Post'
-    import axios from 'axios'
+import {React, useEffect, useState} from 'react'
+import Post from './Post'
+import axios from 'axios'
 
-    const Posts = ({searchValue}) => {
-        const server = import.meta.env.VITE_SERVER_ADDRESS
-        const [posts, setPosts] = useState([])
+const Posts = ({searchValue}) => {
+    const server = import.meta.env.VITE_SERVER_ADDRESS
+    const [posts, setPosts] = useState([])
 
+    useEffect(() => {
         const getAll = async () => {
             try {
                 const response = await axios.get(`${server}/board-all`)
@@ -15,30 +16,30 @@
             }
         }
 
-        useEffect(() => {
-            getAll()
-        }, [posts])
+        getAll()
+    }, [])
+    
 
-        const searchFilter = () => {
-            if(searchValue && searchValue !== '') {
-                return (
-                    posts.filter(post => {
+    const searchFilter = () => {
+        if(searchValue) {
+            return (
+                posts.filter(post => {
 
-                        return post.title.includes(searchValue)
-                    })
-                )
-            } else return posts
-        }
-
-        const filteredValue = searchFilter()
-
-        return (
-            <>
-                {filteredValue.slice().reverse().map((post) => (
-                    <Post key={post.id} post={post}/>
-                ))}
-            </>
-        )
+                    return post.title.includes(searchValue)
+                })
+            )
+        } else return posts
     }
 
-    export default Posts
+    const filteredValue = searchFilter()
+
+    return (
+        <>
+            {filteredValue.slice().reverse().map((post) => (
+                <Post key={post.id} post={post}/>
+            ))}
+        </>
+    )
+}
+
+export default Posts
