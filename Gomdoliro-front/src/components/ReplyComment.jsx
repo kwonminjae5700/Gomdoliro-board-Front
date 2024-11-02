c98a07b33d6d5cc996e55df4e4ac40bb16f650c2
kwon5700 <kwon777071@gmail.com>
2024-11-03 00:10:26 +0900

    Feat : 대댓글 컴포넌트 퍼블리싱

--- Gomdoliro-front/src/components/ReplyComment.jsx
+++ Gomdoliro-front/src/components/ReplyComment.jsx
@@ -1,31 +1,15 @@
-import {React, useState} from 'react';
-import axios from 'axios'
-import '../styles/comment.css';
+import {React, useState} from 'react'
 import user_icon from '../assets/user_icon.png';
-import plus from '../assets/plus.svg'
-import arrow from '../assets/arrow.svg'
 import seroJum from '../assets/seroJum.png';
-import CommentButton from '../components/CommentButton'
-import Modal from '../components/Modal'
-import ReplyComment from '../components/ReplyComment'
+import Modal from '../components/Modal';
+import '../styles/replyComment.css'
 
-const Comment = ({commentId, writer, content, date, boardId, setComDel}) => {
-    const server = import.meta.env.VITE_SERVER_ADDRESS
+const ReplyComment = ({commentId, writer, content, date}) => {
     const [buttonDel, setButtonDel] = useState(false)
     const [modalOpen, setModal] = useState(false)
 
-    const commentDel = async () => {
-        try {
-            await axios.delete(`${server}/board/${boardId}/comments/${commentId}`)
-        } catch(error) {
-            console.log("Error : ", error)
-        }
-
-        setComDel(commentId)
-    }
-
     return (
-        <div className="commentBox">
+        <div className="replyBox">
             {modalOpen && 
                 <Modal 
                     text={"댓글"}
@@ -56,18 +40,8 @@ const Comment = ({commentId, writer, content, date, boardId, setComDel}) => {
             <div className="commentContent">
                 {content}
             </div>
-            <div className="funcComment">
-                <div className="addComment">
-                    <img src={plus} alt="plus"/>
-                    <span>답글 달기</span>
-                </div>
-                <div className="seeComment">
-                    <img src={arrow} alt="arrow"/>
-                    <span>답글 열기</span>
-                </div>
-            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Comment;
+export default ReplyComment
