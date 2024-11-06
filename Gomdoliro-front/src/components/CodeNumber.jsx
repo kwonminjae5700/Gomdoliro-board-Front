10f56d83a1f2c62b0e9a714e3224fbe93378cd9a
kwon5700 <kwon777071@gmail.com>
2024-11-07 03:23:50 +0900

    Fix : 새 비밀번호 창 렌더링 안 되던 문제 해결

--- Gomdoliro-front/src/components/CodeNumber.jsx
+++ Gomdoliro-front/src/components/CodeNumber.jsx
@@ -1,31 +1,61 @@
-import React, { useState, useRef } from 'react'
-import '../styles/codenumber.css'
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import '../styles/codenumber.css';
 
-const CodeNumber = ({email, setCodeCnt}) => {
-    const [code, setCode] = useState(["", "", "", "", "", ""]); // 6자리 예제
-    const inputs = useRef([]);
+const CodeNumber = ({ email, setCodeCnt, send, setButtonDo, setPasswordPage }) => {
+    const server = import.meta.env.VITE_SERVER_ADDRESS;
+    const [codeArrary, setCode] = useState(["", "", "", "", "", ""])
+    const inputs = useRef([])
+    const [canSend, setCanSend] = useState(false)
+
+    useEffect(() => {
+        if(send) {
+            setPasswordPage(true)
+            setButtonDo('완료')
+        }
+    }, [send])
+
+    const sendCode = async () => {
+        try {
+            let code = codeArrary.join("")
+            console.log(email, code)
+
+            const response = await axios.post(`${server}/email/verifyCode`, {
+                email,
+                code
+            })
+
+            console.log(response.data);
+        } catch(error) {
+            console.error("Error : ", error)
+        }
+    };
 
     const inputCode = (value, index) => {
         if(/^[0-9]$/.test(value) || value === "") {
-            let newCode = [...code];
-            newCode[index] = value;
-            setCode(newCode);
+            let newCode = [...codeArrary]
+            newCode[index] = value
+            setCode(newCode)
 
             if(value && index < inputs.current.length - 1) {
-                inputs.current[index + 1].focus();
+                inputs.current[index + 1].focus()
             }
 
             if(newCode.every((char) => char !== "")) {
                 setCodeCnt(true)
-            } else setCodeCnt(false)
+                setCanSend(true)
+            } else {
+                setCodeCnt(false)
+                setCanSend(false)
+            }
         }
-    };
+    }
 
     const backSpaceInput = (e, index) => {
-        if (e.key === "Backspace" && !code[index] && index > 0) {
-            inputs.current[index - 1].focus();
+        if(e.key === "Backspace" && !codeArrary[index] && index > 0) {
+            inputs.current[index - 1].focus()
         }
-    };
+    }
 
     return (
         <div className="code-container">
@@ -33,7 +63,7 @@ const CodeNumber = ({email, setCodeCnt}) => {
             <div className="inputCodeBox">
                 <span>인증번호 입력</span>
                 <div className="inputCode">
-                    {code.map((value, index) => (
+                    {codeArrary.map((value, index) => (
                         <input
                             key={index}
                             type="text"
@@ -49,6 +79,6 @@ const CodeNumber = ({email, setCodeCnt}) => {
             </div>
         </div>
     );
-}
+};
 
-export default CodeNumber
+export default CodeNumber;
--- Gomdoliro-front/src/pages/FindPWPage.jsx
+++ Gomdoliro-front/src/pages/FindPWPage.jsx
@@ -1,24 +1,25 @@
-import {React, useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import axios from 'axios'
-import FindPWInput from '../components/FindPWInput'
-import CodeNumber from '../components/CodeNumber'
-import '../styles/login.css'
+import {React, useState, useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import FindPWInput from '../components/FindPWInput';
+import CodeNumber from '../components/CodeNumber';
+import '../styles/login.css';
 
 const FindPWPage = () => {
     const server = import.meta.env.VITE_SERVER_ADDRESS
     const [email, setEmail] = useState('')
     const [nickname, setNickname] = useState('')
-    const [input, setInput] = useState(false)
+    const [input, setInput] = useState(false);
     const [buttonDo, setButtonDo] = useState('다음')
     const [codeCnt, setCodeCnt] = useState(false)
+    const [send, setSend] = useState(false)
+    const [passwordPage, setPasswordPage] = useState(false)
     const nav = useNavigate()
-
+    
     const testEmail = (word) => {
         let testWord = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-
         return testWord.test(word)
-    }
+    };
 
     const emailNumber = async () => {
         try {
@@ -28,7 +29,7 @@ const FindPWPage = () => {
         } catch(error) {
             console.error("Error : ", error)
         }
-    }
+    };
 
     let next = 0
     let buttonStyle = {
@@ -38,7 +39,7 @@ const FindPWPage = () => {
     if(email.length > 0 && nickname.length > 0 && buttonDo !== '인증하기' || codeCnt) {
         buttonStyle = {
             backgroundColor : "#1F8BFF"
-        }
+        };
         next = 1
     } else {
         next = 0
@@ -57,38 +58,59 @@ const FindPWPage = () => {
                         input={input}
                     />
                 }
-                {buttonDo === '인증하기' && 
+                {buttonDo === '인증하기' && !passwordPage && (
                     <CodeNumber 
                         email={email}
                         setCodeCnt={setCodeCnt}
+                        send={send}
+                        setButtonDo={setButtonDo}
+                        setPasswordPage={setPasswordPage}
                     />
-                }
+                )}
+                {passwordPage && (
+                    <div>
+                        <div className="login-input">
+                            <input 
+                                type="password"    
+                                placeholder="새 비밀번호"
+                            />
+                        </div>
+                        <div className="login-input">
+                            <input 
+                                type="password" 
+                                placeholder="새 비밀번호 확인"
+                            />
+                        </div>
+                        <div className="RePassword">비밀번호를 재설정해주세요.</div>
+                    </div>
+                )}
                 <button style={buttonStyle} onClick={() => {
                     if(next && buttonDo==='다음') {
                         if(testEmail(email)) {
-                            setInput(true)
-                            setButtonDo('이메일 전송')
-                        }
-                        else {
-                            alert("이메일 형식이 올바르지 않습니다.")
-                            setEmail('')
-                            setNickname('')
+                            setInput(true);
+                            setButtonDo('이메일 전송');
+                        } else {
+                            alert("이메일 형식이 올바르지 않습니다.");
+                            setEmail('');
+                            setNickname('');
                         }
                     } else if(buttonDo === '이메일 전송') {
-                        emailNumber()
-                        setButtonDo('인증하기')
+                        emailNumber();
+                        setButtonDo('인증하기');
                     } else if(buttonDo === '인증하기') {
-
+                        setSend(true);
+                    } else if(buttonDo === '완료') {
+                        
                     }
                 }}>{buttonDo}</button>
                 <div className="login-func">
                     <h3 onClick={() => {
-                        nav("/signup")
+                        nav("/signup");
                     }}>로그인하기</h3>
                 </div>
             </main>
         </div>
-    )
-}
+    );
+};
 
-export default FindPWPage
+export default FindPWPage;
