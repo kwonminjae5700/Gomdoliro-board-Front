4ee26ae0198480f0d0f1cac40b5a025e04025721
kwon5700 <kwon777071@gmail.com>
2024-11-06 19:39:08 +0900

    Feat : 대댓글 삭제 기능

--- Gomdoliro-front/src/components/ReplyComment.jsx
+++ Gomdoliro-front/src/components/ReplyComment.jsx
@@ -1,13 +1,24 @@
 import {React, useState} from 'react'
+import axios from 'axios'
 import '../styles/replyComment.css'
 import user_icon from '../assets/user_icon.png';
 import seroJum from '../assets/seroJum.png';
 import Modal from '../components/Modal';
 import CommentButton from '../components/CommentButton'
 
-const ReplyComment = ({writer, content, date}) => {
+const ReplyComment = ({boardId, commentId, recommentsId, writer, content, date, setReDel}) => {
+    const server = import.meta.env.VITE_SERVER_ADDRESS
     const [buttonDel, setButtonDel] = useState(false)
     const [modalOpen, setModal] = useState(false)
+
+    const reCommentDel = async () => {
+        try {
+            await axios.delete(`${server}/board/${boardId}/comments/${commentId}/recomment/${recommentsId}`)
+            setReDel(true)
+        } catch(error) {   
+            console.error("Error : ", error)
+        }
+    }
 
     return (
         <div className="replyBox">
@@ -15,7 +26,7 @@ const ReplyComment = ({writer, content, date}) => {
                 <Modal 
                     text={"댓글"}
                     onClose={() => setModal(false)}
-                    delIsFinish={() => commentDel()}
+                    delIsFinish={() => reCommentDel()}
                 />}
             <span className="commentRow">
                 <img src={user_icon} alt="icon" className="userIcon" />
